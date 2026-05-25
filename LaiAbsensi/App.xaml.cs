4984b21181c8b987b197bfd711b46364d2e50f58
mtalifun <mtalifun@gmail.com>
2026-05-25 14:09:47 +0700

    end

--- LaiAbsensi/App.xaml.cs
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="utf-8" ?>
-<Application xmlns="http://schemas.microsoft.com/dotnet/2021/maui" xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml" x:Class="LaiAbsensi.App">
-    <Application.Resources>
-        <ResourceDictionary>
-            <Color x:Key="Primary">#ea580c</Color>
-        </ResourceDictionary>
-    </Application.Resources>
-</Application>
--- LaiAbsensi/AppShell.xaml.cs
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" ?>
-<Shell xmlns="http://schemas.microsoft.com/dotnet/2021/maui" xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml" x:Class="LaiAbsensi.AppShell" Shell.FlyoutBehavior="Disabled">
-    <TabBar>
-        <ShellContent Title="Absen" Icon="home.png" ContentTemplate="{DataTemplate local:Pages.AbsenPage}" Route="absen"/>
-        <ShellContent Title="Riwayat" Icon="list.png" ContentTemplate="{DataTemplate local:Pages.HistoryPage}" Route="history"/>
-        <ShellContent Title="Setting" Icon="settings.png" ContentTemplate="{DataTemplate local:Pages.SettingsPage}" Route="settings"/>
-    </TabBar>
-</Shell>
